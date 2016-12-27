fb26d5f40f38698401f71de0975a45aa8ee2f849
test <test@test.com>
2016-12-27 22:43:41 +0800

    1227

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,48 @@
 <template>
-	<div>
-		这里是goods
+	<div class="goods">
+		<div class="menu-wrapper"></div>
+		<div class="foods-wrapper"></div>
 	</div>
+
 </template>
 
 <script>
+const ERR_OK = 0;
 export default {
+	data() {
+		return {
+			goods: []
+		};
+	},
+	props: {
+		seller: {
+			type: Object
+		}
+	},
+	created() {
+		this.$http.get('/api/goods').then((response) => {
+			response = response.body;
+			if (response.errno === ERR_OK) {
+				this.goods = response.data;
+			}
+		});
+	}
 };
 </script>
 
-<style>
+<style lang="stylus">
+.goods
+	display flex
+	position:absolute
+	top 174px
+	width 100%
+	bottom 46px
+	overflow hidden
+	.menu-wrapper
+		flex 0 0 80px
+		width 80px
+		background #f3f5f7
+	.foods-wrapper
+		flex 1
+
 </style>
