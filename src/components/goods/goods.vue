d23f83ef2fb8c915eaa4f36546cb4e287747f0f8
test <test@test.com>
2017-01-01 00:14:44 +0800

    20170101

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" v-el:menu-wrapper>
 			<ul>
-				<li v-for="item in goods" class="menu-item">
+				<li v-for="item in goods" class="menu-item" :class="{'current' : currentIndex === $index}">
 					<span class="text border-1px">
 						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
 					</span>
@@ -10,25 +10,23 @@
 			</ul>
 
 		</div>
-		<div class="foods-wrapper">
+		<div class="foods-wrapper" v-el:foods-wrapper>
 			<ul>
-				<li v-for="item in goods" class="food-list">
+				<li v-for="item in goods" class="food-list food-list-hook">
 				<h1 class="title">{{item.name}}</h1>
 				<ul>
-					<li v-for="food in item.foods" class="food-item">
+					<li v-for="food in item.foods" class="food-item border-1px">
 						<div class="icon">
-							<img :src="food.icon" />
+							<img width="57" height="57" :src="food.icon" />
 						</div>
 						<div class="content">
 							<h2 class="name">{{food.name}}</h2>
 							<p class="desc">{{food.description}}</p>
 							<div class="extra">
-								<span>月售{{food.sellCount}}份</span>
-								<span>好评率{{food.rating}}%</span>
+								<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
 							</div>
 							<div class="price">
-								<span>￥{{food.price}}</span>
-								<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								<span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 							</div>
 						</div>
 					</li>
@@ -39,17 +37,31 @@
 
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
 const ERR_OK = 0;
 export default {
 	data() {
 		return {
-			goods: []
+			goods: [],
+			listHeight: [],
+			scrollY: 0
 		};
 	},
 	props: {
 		seller: {
 			type: Object
+		}
+	},
+	computed: {
+		currentIndex() {
+			for (let i = 0; i < this.listHeight.length; i++) {
+				let height1 = this.listHeight[i];
+				let height2 = this.listHeight[i + 1];
+				if (!height2 || ( this.scrollY >= height1 && this.scrollY < height2)) {
+					return i;
+				}
+			}
 		}
 	},
 	created() {
@@ -58,8 +70,34 @@ export default {
 			response = response.body;
 			if (response.errno === ERR_OK) {
 				this.goods = response.data;
+				this.$nextTick(() => {
+					this._initScroll();
+					this._calculateHeight();
+				});
 			}
 		});
+	},
+	methods: {
+		_initScroll() {
+			this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+			this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+				probeType: 3
+			});
+
+			this.foodsScroll.on('scroll', (pos) => {
+				this.scrollY = Math.abs(Math.round(pos.y));
+			});
+		},
+		_calculateHeight() {
+			let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+			let height = 0;
+			this.listHeight.push( height );
+			for (let i = 0; i < foodList.length; i++) {
+				let item = foodList[i];
+				height += item.clientHeight;
+				this.listHeight.push(height);
+			}
+		}
 	}
 };
 </script>
@@ -83,6 +121,14 @@ export default {
 			width 56px
 			line-height 14px
 			padding 0 12px
+			&.current
+				background #fff
+				position relative
+				margin-top -1px
+				font-weight 700
+				z-index 10
+				.text
+					border-none()
 			.icon
 				display:inline-block
 				vertical-align:top
@@ -110,5 +156,53 @@ export default {
 
 	.foods-wrapper
 		flex 1
+		.title
+			padding-left 14px
+			height 2
+			line-height 26px
+			border-left 2px solid #d9dde1
+			font-size 12px
+			color rgb(147,153,159)
+			background #f3f5f7
+		.food-item
+			display flex
+			margin 18px
+			padding-bottom 18px
+			border-1px(rgba(7,17,27,0.1))
+			&:last-child
+				border-none()
+				margin-bottom 0
+			.icon
+				flex 0 0 57px
+				margin-right 10px
+			.content
+				flex 1
+				.name
+					margin 2px 0 8px 0
+					height 14px
+					line-height 14px
+					font-size 14px
+					color rgb(7,17,27)
+				.desc,.extra
+					line-height 10px
+					font-size 10px
+					color rgb(147,153,159)
+				.desc
+					margin-bottom 8px
+					line-height 12px
+				.extra
+					.count
+						margin-right 12px
+				.price
+					font-weight 700
+					line-height 24px
+					.now
+						margin-right 8px
+						font-size 14px
+						color rgb(240,20,20)
+					.old
+						text-decoration line-through
+						font-size 10px
+						color rgb(147,153,159)
 
 </style>
